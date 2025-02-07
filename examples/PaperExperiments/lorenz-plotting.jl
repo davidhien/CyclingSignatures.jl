c6cb1470bc480a2373e06c972669af37e95d741d
David <david.hien@tum.de>
2025-02-07 13:18:42 +0100

    plot a segment

--- examples/PaperExperiments/lorenz-plotting.jl
+++ examples/PaperExperiments/lorenz-plotting.jl
@@ -21,3 +21,14 @@ plotRanksWithLegend(lorenzResults, lorenz_r; gl=g11)
 plotSignaturesWithLegend(lorenzResults, 1, lorenz_r; gl=g12)
 plotSignaturesWithLegend(lorenzResults, 2, lorenz_r; gl=g21, legend_kwargs=(;nbanks=5))
 plotSubspaceInclusion(lorenzResults, 1, 2, lorenz_r; gp=g22, axis_kwargs=(;limits=(.5,3.5,.8,2.2)))
+
+
+# plot a segment
+trajLorenz = getTrajectory(getTrajectorySpace(lorenzResults[10].experiment))
+sig1_l,_ = subspaceFrequencyMatrix(lorenzResults, 1, lorenz_r)
+sig_1_range = getSignatureRanges(lorenzResults[10], sig1_l[1], lorenz_r)
+
+background_pts  = get(trajLorenz, 1:10000)[1:3,:] 
+segment_pts = get(trajLorenz, sig_1_range[1])[1:3,:]
+scatter(background_pts, color=:black, markersize=1)
+scatter!(segment_pts, color=:red)
